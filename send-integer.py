cf2d5e3ad0209585f7b8e3378bea7dd8b0f5f2a3
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-07-30 11:40:57 +0100

    finding day

--- send-integer.py
+++ send-integer.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 @app.route("/count", methods=["GET"])
 def count():
     in_count = 0
+    today = datetime.today().strftime("%Y-%m-%d") 
     with open("people_count.csv", "r") as count_file:
         for line in count_file:
-            if datetime.today().strftime("%Y-%m-%d") in line and "IN" in line:
+            if today in line and "IN" in line:
                 in_count += 1
     return jsonify({"value": in_count})
 
