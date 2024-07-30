4f9d484caa875cdf5a88cf2b95e75a71c8655f1e
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-07-30 10:34:38 +0100

    interger sending code

---
+++ send-integer.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/count", methods=["GET"])
+def count():
+    in_count = 0
+    with open("people_count.csv", "r") as count_file:
+        for line in count_file:
+            if datetime.today().strftime("%Y-%m-%d") in line and "IN" in line:
+                in_count += 1
+    return jsonify({"value": in_count})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
