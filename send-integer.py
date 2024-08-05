ae9febd3c90a98e452843a45cada48bcc6b2608d
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-08-05 10:44:37 +0100

    sending info from broken up files

--- send-integer.py
+++ send-integer.py
@@ -1,16 +1,21 @@
-from flask import Flask, jsonify
+import os
 from datetime import datetime
 
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
+DATA_PATH = "/home/dolica/people-counter-cth/data"
 
 
 @app.route("/count", methods=["GET"])
 def count():
     in_count = 0
     today = datetime.today().strftime("%Y-%m-%d")
-    with open("people_count.csv", "r") as count_file:
+    data_file_name = today + ".csv"
+    with open(os.path.join(DATA_PATH, data_file_name), "r") as count_file:
         for line in count_file:
-            if today in line and "IN" in line:
+            if "IN" in line:
                 in_count += 1
     return jsonify({"value": in_count})
 
